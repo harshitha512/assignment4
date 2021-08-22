45f6dbad282ce1707a38eab66f74da05a5cc0604
harshitha512 <harshitha.yellasiri@gmail.com>
2021-08-22 12:06:15 +0530

    update

--- assignment 4/script.js
+++ assignment 4/script.js
@@ -12,5 +12,10 @@ names[9]="jim";
 
 
 for (var i = 0; i < names.length; i++) {
-	console.log(names[i]);
+	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+        console.log("Goodbye "+ names[i])
+	}
+	else{
+		console.log("Hello "+ names[i])
+	}
 }
